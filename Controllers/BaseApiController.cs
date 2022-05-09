62e1d6ae123b16b02b6472ba788c5f347c445b60
Vitaly Posadnev <elemabor@gmail.com>
2022-05-10 02:42:06 +0300

    feat(controllers): create BaseApiController

---
+++ Controllers/BaseApiController.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace ProxyApp.Controllers
+{
+    [EnableCors("*", "*", "*")]
+    public class BaseApiController : ApiController
+    {
+        protected readonly string _baseUrl = "https://patient.simplex48.ru/";
+
+        protected string GetBodyFromRequest()
+        {
+            var requestString = string.Empty;
+
+            using (var reader = new StreamReader(HttpContext.Current.Request.InputStream))
+            {
+                requestString = reader.ReadToEnd();
+            }
+
+            return requestString;
+        }
+    }
+}
