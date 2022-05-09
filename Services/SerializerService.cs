48b0f9eac9e6cbd3eb838623995a916dde369495
Vitaly Posadnev <elemabor@gmail.com>
2022-05-10 02:49:00 +0300

    feat(services): create serializer service

---
+++ Services/SerializerService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Web;
+using Newtonsoft.Json;
+using ProxyApp.DTOs;
+
+namespace ProxyApp.Services
+{
+    public static class SerializerService
+    {
+        public static NameValueCollection ConvertFormDataToDict(string formData)
+        {
+            try
+            {
+                return HttpUtility.ParseQueryString(formData);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public static object DeserializeObject(string json)
+        {
+            try
+            {
+                return JsonConvert.DeserializeObject(json);
+            }
+            catch (Exception)
+            {
+                try
+                {
+                    return JsonConvert.DeserializeObject<ErrorResponse>(json);
+                }
+                catch (Exception)
+                {
+                    return null;
+                }
+            }
+        }
+
+        public static string SerializeObject(object item)
+        {
+            try
+            {
+                return JsonConvert.SerializeObject(item);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
