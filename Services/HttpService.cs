d5716ab5b9a2da8d27eb6b8ccd1d5386458ac7a1
Vitaly Posadnev <elemabor@gmail.com>
2022-05-10 02:45:47 +0300

    feat(services): create http service for post requests

---
+++ Services/HttpService.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using ProxyApp.DTOs;
+
+namespace ProxyApp.Services
+{
+    public enum ContentType
+    {
+        JSON,
+        FormData
+    }
+
+    public static class HttpService
+    {
+        private static readonly HttpClient _client = new HttpClient();
+
+        public static async Task<ResponseData> Post(string url, string payload, ContentType contentType)
+        {
+            var response = string.Empty;
+
+            var httpContent = new StringContent(payload, Encoding.UTF8,
+                contentType == ContentType.JSON ? "application/json" : "application/x-www-form-urlencoded");
+            var result = await _client.PostAsync(url, httpContent);
+
+            if (result != null) response = await result.Content.ReadAsStringAsync();
+
+            return new ResponseData
+            {
+                StatusCode = (int)result.StatusCode,
+                Body = response
+            };
+        }
+    }
+}
