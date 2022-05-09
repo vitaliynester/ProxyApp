a6c55d4d9f1823481d70569211fc1a2777890c52
Vitaly Posadnev <elemabor@gmail.com>
2022-05-10 02:54:35 +0300

    feat(app): create user profile context with controller and model

---
+++ Controllers/UserProfilesController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ProxyApp.Data;
+using ProxyApp.Models;
+
+namespace ProxyApp.Controllers
+{
+    public class UserProfilesController : ApiController
+    {
+        private UserProfileContext db = new UserProfileContext();
+
+        // GET: api/UserProfiles
+        public IQueryable<UserProfile> GetUserProfiles()
+        {
+            return db.UserProfiles;
+        }
+
+        // GET: api/UserProfiles/5
+        [ResponseType(typeof(UserProfile))]
+        public async Task<IHttpActionResult> GetUserProfile(int id)
+        {
+            UserProfile userProfile = await db.UserProfiles.FindAsync(id);
+            if (userProfile == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(userProfile);
+        }
+
+        // PUT: api/UserProfiles/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutUserProfile(int id, UserProfile userProfile)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != userProfile.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(userProfile).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserProfileExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/UserProfiles
+        [ResponseType(typeof(UserProfile))]
+        public async Task<IHttpActionResult> PostUserProfile(UserProfile userProfile)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.UserProfiles.Add(userProfile);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = userProfile.ID }, userProfile);
+        }
+
+        // DELETE: api/UserProfiles/5
+        [ResponseType(typeof(UserProfile))]
+        public async Task<IHttpActionResult> DeleteUserProfile(int id)
+        {
+            UserProfile userProfile = await db.UserProfiles.FindAsync(id);
+            if (userProfile == null)
+            {
+                return NotFound();
+            }
+
+            db.UserProfiles.Remove(userProfile);
+            await db.SaveChangesAsync();
+
+            return Ok(userProfile);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool UserProfileExists(int id)
+        {
+            return db.UserProfiles.Count(e => e.ID == id) > 0;
+        }
+    }
+}
