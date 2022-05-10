2ed67956a7c62572a670123dfd5f0a0f98d5d95a
Vitaly Posadnev <elemabor@gmail.com>
2022-05-10 03:04:00 +0300

    feat(mobile-controller): update logic for mobile controller

--- Controllers/MobileController.cs
+++ Controllers/MobileController.cs
@@ -1,13 +1,76 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
+using System.Web.Http.Description;
+using ProxyApp.DTOs;
+using ProxyApp.Models;
+using ProxyApp.Services;
 
 namespace ProxyApp.Controllers
 {
+    [RoutePrefix("api/Mobile")]
     public class MobileController : BaseApiController
     {
+        [HttpPost]
+        [ResponseType(typeof(PhoneConfirmationRequestResponse))]
+        [Route("PhoneConfirmation")]
+        public async Task<IHttpActionResult> PhoneConfirmation()
+        {
+            var requestString = GetBodyFromRequest();
+
+            var url = _baseUrl + "/api/Mobile/PhoneConfirmation";
+
+            var response = await HttpService.Post(url, requestString,
+                requestString.Contains("\"") ? ContentType.JSON : ContentType.FormData);
+
+            try
+            {
+                return new JsonHttpStatusResult<PhoneConfirmationRequestResponse>(
+                    PhoneConfirmationRequestResponse.FromJson(response.Body), response.StatusCode, this);
+            }
+            catch (Exception)
+            {
+                return new JsonHttpStatusResult<ErrorResponse>(ErrorResponse.FromJson(response.Body),
+                    response.StatusCode, this);
+            }
+        }
+
+        [HttpPost]
+        [ResponseType(typeof(CodeConfirmationRequestResponse))]
+        [Route("CodeConfirmation")]
+        public async Task<IHttpActionResult> CodeConfirmation()
+        {
+            var requestString = GetBodyFromRequest();
+
+            var url = _baseUrl + "/api/Mobile/CodeConfirmation";
+
+            var response = await HttpService.Post(url, requestString,
+                requestString.Contains("\"") ? ContentType.JSON : ContentType.FormData);
+
+            try
+            {
+                var responseModel = CodeConfirmationRequestResponse.FromJson(response.Body);
+
+                try
+                {
+                    var userProfile = new UserProfile();
+                    userProfile.Code = responseModel.Code;
+                    userProfile.MedOrgId = responseModel.MedOrgId;
+                    userProfile.Phone = responseModel.Phone;
+                    await UserProfileService.Add(userProfile);
+                }
+                catch (Exception)
+                {
+                }
+
+                return new JsonHttpStatusResult<CodeConfirmationRequestResponse>(
+                    CodeConfirmationRequestResponse.FromJson(response.Body), response.StatusCode, this);
+            }
+            catch (Exception)
+            {
+                return new JsonHttpStatusResult<ErrorResponse>(ErrorResponse.FromJson(response.Body),
+                    response.StatusCode, this);
+            }
+        }
     }
 }
