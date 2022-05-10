49951288dadc7969508f759f823320356dc9b5cb
Vitaly Posadnev <elemabor@gmail.com>
2022-05-10 03:05:28 +0300

    feat(root-controller): update logic for root controller

--- Controllers/RootController.cs
+++ Controllers/RootController.cs
@@ -1,13 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
+using System.Web.Http.Description;
+using Newtonsoft.Json;
+using ProxyApp.DTOs;
+using ProxyApp.Services;
 
 namespace ProxyApp.Controllers
 {
+    [RoutePrefix("")]
     public class RootController : BaseApiController
     {
+        [HttpPost]
+        [ResponseType(typeof(TokenRootResponse))]
+        [Route("Token")]
+        public async Task<IHttpActionResult> Authorization()
+        {
+            var requestString = GetBodyFromRequest();
+
+            var url = _baseUrl + "/Token";
+
+            var response = await HttpService.Post(url, requestString,
+                requestString.Contains("\"") ? ContentType.JSON : ContentType.FormData);
+
+            try
+            {
+                return new JsonHttpStatusResult<TokenRootResponse>(
+                    JsonConvert.DeserializeObject<TokenRootResponse>(response.Body), response.StatusCode, this);
+            }
+            catch (Exception)
+            {
+                return new JsonHttpStatusResult<TokenRootErrorResponse>(TokenRootErrorResponse.FromJson(response.Body),
+                    response.StatusCode, this);
+            }
+        }
     }
 }
