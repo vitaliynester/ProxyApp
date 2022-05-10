09293faecfbebbafc55d668fb922dd1b3312ed0b
Vitaly Posadnev <elemabor@gmail.com>
2022-05-10 14:13:52 +0300

    feat(user-profile-model): add property name for response

--- Models/UserProfile.cs
+++ Models/UserProfile.cs
@@ -1,23 +1,41 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ProxyApp.Models
 {
     public class UserProfile
     {
-        [Column("user_profile_id")] public int ID { get; set; }
+        [JsonProperty("id")]
+        [Column("user_profile_id")] 
+        public int ID { get; set; }
 
-        [Column("user_profile_first_name")] public string FirstName { get; set; }
+        [JsonProperty("first_name")]
+        [Column("user_profile_first_name")] 
+        public string FirstName { get; set; }
 
-        [Column("user_profile_surname")] public string Surname { get; set; }
+        [JsonProperty("surname")]
+        [Column("user_profile_surname")] 
+        public string Surname { get; set; }
 
-        [Column("user_profile_patronymic")] public string Patronymic { get; set; }
+        [JsonProperty("patronymic")]
+        [Column("user_profile_patronymic")] 
+        public string Patronymic { get; set; }
 
-        [Column("user_profile_phone")] public string Phone { get; set; }
+        [JsonProperty("phone")]
+        [Column("user_profile_phone")] 
+        public string Phone { get; set; }
 
-        [Column("user_profile_code")] public string Code { get; set; }
+        [JsonProperty("code")]
+        [Column("user_profile_code")] 
+        public string Code { get; set; }
 
-        [Column("user_profile_guid")] public string GUID { get; set; }
+        [JsonProperty("guid")]
+        [Column("user_profile_guid")] 
+        public string GUID { get; set; }
 
-        [Column("user_profile_med_org_id")] public int MedOrgId { get; set; }
+
+        [JsonProperty("med_org_id")]
+        [Column("user_profile_med_org_id")] 
+        public int MedOrgId { get; set; }
     }
 }
