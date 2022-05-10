8cde09ff9b70337043dbcf38f672ca27ee509dbc
Vitaly Posadnev <elemabor@gmail.com>
2022-05-10 03:06:12 +0300

    feat(user-profile-controller): add route for get all user profile by phone and medOrgId

--- Controllers/UserProfilesController.cs
+++ Controllers/UserProfilesController.cs
@@ -1,120 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
-using System.Data.Entity.Infrastructure;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using System.Web.Http;
-using System.Web.Http.Description;
-using ProxyApp.Data;
+using ProxyApp.DTOs;
 using ProxyApp.Models;
+using ProxyApp.Services;
 
 namespace ProxyApp.Controllers
 {
-    public class UserProfilesController : ApiController
+    public class UserProfilesController : BaseApiController
     {
-        private UserProfileContext db = new UserProfileContext();
-
-        // GET: api/UserProfiles
-        public IQueryable<UserProfile> GetUserProfiles()
+        [HttpPost]
+        [Route("api/UserProfiles")]
+        public IQueryable<UserProfile> GetAll(UserProfileRequest request)
         {
-            return db.UserProfiles;
-        }
-
-        // GET: api/UserProfiles/5
-        [ResponseType(typeof(UserProfile))]
-        public async Task<IHttpActionResult> GetUserProfile(int id)
-        {
-            UserProfile userProfile = await db.UserProfiles.FindAsync(id);
-            if (userProfile == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(userProfile);
-        }
-
-        // PUT: api/UserProfiles/5
-        [ResponseType(typeof(void))]
-        public async Task<IHttpActionResult> PutUserProfile(int id, UserProfile userProfile)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            if (id != userProfile.ID)
-            {
-                return BadRequest();
-            }
-
-            db.Entry(userProfile).State = EntityState.Modified;
-
-            try
-            {
-                await db.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!UserProfileExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return StatusCode(HttpStatusCode.NoContent);
-        }
-
-        // POST: api/UserProfiles
-        [ResponseType(typeof(UserProfile))]
-        public async Task<IHttpActionResult> PostUserProfile(UserProfile userProfile)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            db.UserProfiles.Add(userProfile);
-            await db.SaveChangesAsync();
-
-            return CreatedAtRoute("DefaultApi", new { id = userProfile.ID }, userProfile);
-        }
-
-        // DELETE: api/UserProfiles/5
-        [ResponseType(typeof(UserProfile))]
-        public async Task<IHttpActionResult> DeleteUserProfile(int id)
-        {
-            UserProfile userProfile = await db.UserProfiles.FindAsync(id);
-            if (userProfile == null)
-            {
-                return NotFound();
-            }
-
-            db.UserProfiles.Remove(userProfile);
-            await db.SaveChangesAsync();
-
-            return Ok(userProfile);
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-
-        private bool UserProfileExists(int id)
-        {
-            return db.UserProfiles.Count(e => e.ID == id) > 0;
+            if (request == null) return Enumerable.Empty<UserProfile>().AsQueryable();
+            return UserProfileService.FindAllByPhoneAndMedOrgId(request.Phone, request.MedOrgId);
         }
     }
 }
