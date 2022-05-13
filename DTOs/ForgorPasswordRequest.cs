99f06f4bcdf32092a090f632061d2f20400bcd07
Vitaly Posadnev <elemabor@gmail.com>
2022-05-14 01:23:06 +0300

    feat(dtos): create forgot password request and response

---
+++ DTOs/ForgorPasswordRequest.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace ProxyApp.DTOs
+{
+    public class ForgorPasswordRequest
+    {
+        [JsonProperty("medorgId")]
+        public int MedOrgId { get; set; } = 0;
+        
+        [JsonProperty("phone")]
+        public string Phone { get; set; }
+    }
+}
---
+++ DTOs/ForgorPasswordRequestResponse.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace ProxyApp.DTOs
+{
+    public class ForgorPasswordRequestResponse
+    {
+        [JsonProperty("code")]
+        public string Code { get; set; }
+        [JsonProperty("success")]
+        public bool Success { get; set; }
+
+        public static ForgorPasswordRequestResponse FromJson(string json)
+        {
+            try
+            {
+                var result = JsonConvert.DeserializeObject<ForgorPasswordRequestResponse>(json);
+                return result;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
