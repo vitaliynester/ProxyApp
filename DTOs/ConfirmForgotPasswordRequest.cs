88d91e59d7627f3b198d275683c42c79d4f86e87
Vitaly Posadnev <elemabor@gmail.com>
2022-05-14 01:22:50 +0300

    feat(dtos): create confirm forgot password request and response

---
+++ DTOs/ConfirmForgotPasswordRequest.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace ProxyApp.DTOs
+{
+    public class ConfirmForgotPasswordRequest
+    {
+        [JsonProperty("medorgId")]
+        public int MedOrgId { get; set; } = 0;
+
+        [JsonProperty("phone")]
+        public string Phone { get; set; }
+
+        [JsonProperty("code")]
+        public string Code { get; set; }
+    }
+}
---
+++ DTOs/ConfirmForgotPasswordRequestResponse.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace ProxyApp.DTOs
+{
+    public class ConfirmForgotPasswordRequestResponse
+    {
+        [JsonProperty("password")]
+        public string Password { get; set; }
+
+        [JsonProperty("success")]
+        public bool Success { get; set; }
+
+        public static ConfirmForgotPasswordRequestResponse FromJson(string json)
+        {
+            try
+            {
+                var result = JsonConvert.DeserializeObject<ConfirmForgotPasswordRequestResponse>(json);
+                return result;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+    }
+}
