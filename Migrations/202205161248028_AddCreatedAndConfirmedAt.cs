1abe29f7ee93b0d31a5ecf6bc13d765f15f3d72d
Vitaly Posadnev <elemabor@gmail.com>
2022-05-16 16:39:45 +0300

    build(migrations): create migration for create new fields for user profile model

---
+++ Migrations/202205161248028_AddCreatedAndConfirmedAt.cs
@@ -0,0 +1,16 @@
+﻿namespace ProxyApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+
+    public partial class AddCreatedAndConfirmedAt : DbMigration
+    {
+        public override void Up()
+        {
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
