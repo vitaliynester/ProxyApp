57fbaa57eec8e21f3af4e0ac7576f73a1bac786b
Vitaly Posadnev <elemabor@gmail.com>
2022-06-19 12:42:42 +0300

    build(migrations): create migration for create new column with birthday

---
+++ Migrations/202205301341015_AddBirthdayField.cs
@@ -0,0 +1,18 @@
+﻿namespace ProxyApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddBirthdayField : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.UserProfiles", "user_profile_birthday", c => c.DateTime(nullable: true, precision: 7, storeType: "datetime2"));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.UserProfiles", "user_profile_birthday");
+        }
+    }
+}
