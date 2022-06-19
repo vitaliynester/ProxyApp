24a8171324e7cbe370fca5bee894c515070ca52b
Vitaly Posadnev <elemabor@gmail.com>
2022-06-19 12:43:34 +0300

    feat(dtos): create token root request

---
+++ DTOs/TokenRootRequest.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace ProxyApp.DTOs
+{
+    public class TokenRootRequest
+    {
+        [JsonProperty("grant_type")]
+        [JsonRequired]
+        public string Grant_type { get; set; }
+
+        [JsonProperty("username")]
+        [JsonRequired]
+        public string Username { get; set; }
+
+        [JsonProperty("password")]
+        [JsonRequired]
+        public string Password { get; set; }
+    }
+}
